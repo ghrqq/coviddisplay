4db937d79a6e69749bf35d0d7dc0b9787c6be74f
Theo <theoozdev@gmail.com>
2021-01-20 16:17:15 +0100

    Geolocation removed.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import CountryCard from "../components/CountryCard";
 import GlobalDataDisplayer from "../components/GlobalDataDisplayer";
 import useWindowDimensions from "../tools/useWindowDimensions";
 import MainMiddle from "../components/MainMiddle";
-// import { countryNamer } from "../tools/CountryNamer";
+import { countryNamer } from "../tools/CountryNamer";
 // import axios from "axios";
 
 export default function Home(props) {
@@ -16,10 +16,10 @@ export default function Home(props) {
   const [availableSpace, setAvailableSpace] = useState(Math.floor(width / 330));
 
   const [isSelectedLoading, setisSelectedLoading] = useState("");
-  // const [lang, setlang] = useState("");
+  const [lang, setlang] = useState("");
   const [initial, setinitial] = useState([]);
 
-  // const [details, setdetails] = useState({});
+  const [details, setdetails] = useState({});
 
   // const languageSetter = async () => {
   //   setisRelatedLoading(true);
@@ -30,71 +30,65 @@ export default function Home(props) {
   //   setisRelatedLoading(false);
   // };
 
+  // useEffect(() => {
+  //   async function getUserGeolocationDetails() {
+  //     setisSelectedLoading(true);
+  //     let tempArr = [];
+  //     const langu =
+  //       window.navigator.userLanguage || window.navigator.language || "US";
+
+  //     const languageName =
+  //       langu.length > 0
+  //         ? countryNamer(langu.slice(3, 5).toUpperCase(), "name")
+  //         : countryNamer(langu.toUpperCase(), "name");
+
+  //     setlang({ ab: langu, name: languageName[0] });
+
+  //     tempArr.push(langu.slice(3, 5).toUpperCase());
+
+  //     setinitial(tempArr);
+
+  //     const result = await (
+  //       await fetch(
+  //         "https://geolocation-db.com/json/09ba3820-0f88-11eb-9ba6-e1dd7dece2b8"
+  //       )
+  //     ).json();
+  //     if (result) {
+  //       setdetails(result);
+
+  //       tempArr.push(result.country_code);
+  //       setinitial(tempArr);
+  //       setisSelectedLoading(false);
+  //     } else {
+  //       tempArr.push("DE");
+  //       setinitial(tempArr);
+
+  //       setisSelectedLoading(false);
+  //     }
+  //   }
+
+  //   getUserGeolocationDetails();
+  // }, []);
+
   useEffect(() => {
     setisSelectedLoading(true);
-    console.log("geo 1");
-    async function getUserGeolocationDetails() {
-      console.log("geo 2");
-      let tempArr = [];
-      const langu =
-        window.navigator.userLanguage || window.navigator.language || "US";
-
-      const languageName =
-        langu.length > 0
-          ? countryNamer(langu.slice(3, 5).toUpperCase(), "name")
-          : countryNamer(langu.toUpperCase(), "name");
-
-      setlang({ ab: langu, name: languageName[0] });
-      console.log("geo 3");
-
-      tempArr.push(langu.slice(3, 5).toUpperCase());
-      console.log(tempArr);
-
-      setinitial(tempArr);
-      console.log("geo 4");
-
-      const result = await (
-        await fetch(
-          "https://geolocation-db.com/json/09ba3820-0f88-11eb-9ba6-e1dd7dece2b8"
-        )
-      ).json();
-      if (result) {
-        setdetails(result);
-        console.log("geo 5");
-        tempArr.push(result.country_code);
-      } else {
-        tempArr.push("DE");
-        console.log("geo 6");
-      }
-      setinitial(tempArr);
-
-      setisSelectedLoading(false);
-      console.log("geo 7");
+    if (!countries) {
+      return;
     }
-    console.log("geo 8");
-    getUserGeolocationDetails();
-    console.log("geo 9");
-  }, []);
+    let tempArr = [];
 
-  // useEffect(() => {
-  //   setisSelectedLoading(true);
-  //   if (!countries) {
-  //     return;
-  //   }
-  //   let tempArr = [];
+    let lang =
+      window.navigator.userLanguage || window.navigator.language || "US";
+    if (lang) {
+      tempArr.push(lang.slice(3, 5).toUpperCase());
+    }
 
-  //   let lang =
-  //     window.navigator.userLanguage || window.navigator.language || "US";
-  //   if (lang) {
-  //     tempArr.push(lang.slice(3, 5).toUpperCase());
-  //   }
+    tempArr.push(countries[Math.floor(Math.random() * 100)]["CountryCode"]);
 
-  //   tempArr.push(countries[Math.floor(Math.random() * 100)]["CountryCode"]);
-
-  //   setinitial(tempArr);
-  //   setisSelectedLoading(false);
-  //   return () => {};
-  // }, [countries]);
+    setinitial(tempArr);
+    setisSelectedLoading(false);
+    return () => {};
+  }, [countries]);
 
   if (isSelectedLoading) return <h2>Loading.</h2>;
   return (
